a9f3ccc8cbe76c2fd96b84ae922a3c5f34bcd2ae
peeyush-ohri <peeyush.ohri@accenture.com>
2017-09-18 16:26:55 +0530

    Deleted spec

--- We@Accenture/spec.js
+++
@@ -1,8 +0,0 @@
-describe('Login to We@Accenture', function() {
- it('should add a Name as GURU99', function() {
- browser.get('https://weataccenture-pt.ciotest.accenture.com/');
- element(by.model('yourName')).sendKeys('GURU99');
-  var guru= element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
-expect(guru.getText()).toEqual('Hello GURU99!');
-  });
-});
